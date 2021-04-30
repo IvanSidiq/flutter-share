d24db391ced47ec98e7467adb64f9e52c831bfe3
Ivan Muhammad SIdiq <ivan.sidiq.is@gmail.com>
2021-05-01 03:05:18 +0700

    fix wa intent

--- android/src/main/java/io/flutter/plugins/share/Share.java
+++ android/src/main/java/io/flutter/plugins/share/Share.java
@@ -105,15 +105,15 @@ class Share {
     startActivity(chooserIntent);
   }
 
-  void shareWa(String text, String waNumber) {
+  void shareWa(String text, String subject, String waNumber) {
     if (text == null || text.isEmpty()) {
       throw new IllegalArgumentException("Non-empty text expected");
     }
 
     Intent shareIntent = new Intent();
-    Uri uri = Uri.parse("smsto:" + waNumber);
-    shareIntent.setAction(Intent.ACTION_SENDTO, uri);
-    shareIntent.putExtra("sms_body", text);
+    shareIntent.setAction(Intent.ACTION_SEND);
+    shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+    shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
     shareIntent.setPackage("com.whatsapp");
     shareIntent.setType("text/plain");
     
@@ -134,7 +134,7 @@ class Share {
 
     Intent shareIntent = new Intent();
     if (fileUris.isEmpty()) {
-      shareWa(text, waNumber);
+      shareWa(text, subject, waNumber);
       return;
     } else if (fileUris.size() == 1) {
       shareIntent.setAction(Intent.ACTION_SEND);
